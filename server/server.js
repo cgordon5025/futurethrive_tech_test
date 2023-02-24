4f6e5f849414c45bca02a3827f861ae5eb4aea91
unknown <cgordon5025@gmail.com>
2023-02-24 10:37:25 -0500

    changing some dependencies

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const mongodb = require('mongodb')
+// const mongodb = require('mongodb')
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 
--- server/template/processVideo.js
+++ server/template/processVideo.js
@@ -1,15 +1,15 @@
-const mongodb = require('mongodb')
+// const mongodb = require('mongodb')
 
-const fs = require('fs')
-const db = require('../config/connection');
-const bucket = new mongodb.GridFSBucket(db, { bucketName: "videos" })
-function processVideo(data) {
-    console.log(data)
-    const fileLocation = '~./server/template/sample.mp4'
-    fs.createReadStream(`~./server/template/sample.mp4`).
-        pipe(bucket.openUploadStream('myFile', {
-            chunkSizeBytes: 1048576,
-            metadata: { field: 'myField', value: 'myValue' }
-        }));
-}
-module.exports = processVideo
+// const fs = require('fs')
+// const db = require('../config/connection');
+// const bucket = new mongodb.GridFSBucket(db, { bucketName: "videos" })
+// function processVideo(data) {
+//     console.log(data)
+//     const fileLocation = '~./server/template/sample.mp4'
+//     fs.createReadStream(`~./server/template/sample.mp4`).
+//         pipe(bucket.openUploadStream('myFile', {
+//             chunkSizeBytes: 1048576,
+//             metadata: { field: 'myField', value: 'myValue' }
+//         }));
+// }
+// module.exports = processVideo
